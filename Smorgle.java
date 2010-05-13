2c1ab958521deb10c36a3934da9855052b4de5d1
Jon Spiegel <spiegeljb@yahoo.com>
2010-05-12 23:08:52 -0400

    lol

---
+++ Smorgle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+class Smorgle {
+    public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+        System.out.println("Hello World!"); // Display the string.
+		try
+		{
+			in.nextLine();
+		} catch (Exception e)
+		{
+		
+		}
+    }
+}
+
